4b1466a7add385fda5e191d50ef4c39001a193d0
Juan Pablo Bedoya Urrego <juan.pablo.bedoya02@gmail.com>
2024-03-26 14:02:47 -0500

    Punto Uno Acabado

--- Pokemon..cs
+++ Pokemon..cs
@@ -1,10 +1,14 @@
 using System.Reflection.Metadata;
+//Declaro la interfaz con las metodos que debe llevar
 public interface IPokemon{
     int Ataque();
     int Defensa();
 }
 
+//La clase Pokemon con la interfaz que declare anteriormente
 public class Pokemon:IPokemon{
+    /*Los atratibutos que va a llegar esta clase, las listas son para guardar los ataques y las defensas que
+    debe tener cada objeto que se instancia de esta clase*/
     private Random r = new Random();
     private String nombrePokemon{get; set;}
     private String tipoPokemon{get; set;}
@@ -12,53 +16,53 @@ public class Pokemon:IPokemon{
 
     private List<int> AtaquesPokemon{get; set;} = new List<int>();
     private List<int> DefensasPokemon{get; set;} = new List<int>();
-
+    //El constructor de esta clase en el que se inicializa cada uno de sus atributos
     public Pokemon (String Nombre, String Tipo, int Salud){
         this.nombrePokemon = Nombre;
         this.tipoPokemon = Tipo;
         this.saludPokemon = Salud;
-
+        //Aqui se guarda los ataques con un valor entero desde 0 a 40
         for(int i = 0; i < 3; i++){
             AtaquesPokemon.Add(r.Next(0,40));
         }
+        //y aqui se guardan las defensas 
         for(int i = 0; i < 3; i++){
             DefensasPokemon.Add(r.Next(10,35));
         }
     }
+    /*el metodo ataque en el cual de la lista de ataques se escoje de manera aleatorio un ataque de esta lista 
+    y depues se multiplica por un valor que puede ser 1 o 1.5 esto se hace dejando que la probalidad que toque
+    uno o el otro sea del 50% y despues se retorna de manera entera el valor del ataque multiplicado por este 
+    valor*/
     public int Ataque(){
         int ataque = AtaquesPokemon[r.Next(0, 3)];
         double multiplicadorDaño = r.NextDouble() < 0.5 ? 1 : 1.5; // 50% probabilidad de 1, 50% de 1.5
 
         return (int)(ataque * multiplicadorDaño);
     }
-
+    //Lo mismo que el ataque ya que los valores del 50/50 ya son 0.5 o 1.0
     public int Defensa(){
         int defensa = DefensasPokemon[r.Next(0, 2)];
         double multiplicadorDefensa = r.NextDouble() < 0.5 ? 1 : 0.5;
         return (int)(defensa * multiplicadorDefensa);
     }
-
-    public void mostrarAtaquesBase(){
-        foreach(int At in AtaquesPokemon){
-            Console.WriteLine($"Ataque Base: {At}");
-        }
-        foreach(int Df in DefensasPokemon){
-            Console.WriteLine($"Defensa Base: {Df}");
-        }
-    }
-
+    //Este metodo se usa para poder ir modificando la salud de cada pokemon a medida que va a avanzado la pelea
     public void SetSalud(int Salud){
         saludPokemon = Salud;
     }
-
+    /*Este se usa para poder ver el valor que tiene el atribito nombrePokemon ya que no se puede acceder 
+    directamente a este atributo por que es privado*/
     public String GetNombre(){
         return nombrePokemon;
     }
-
+    //Lo mismo que el GetNombre
     public int GetSalud(){
         return saludPokemon;
     }
-
+    /*Este metodo se usa para calcular el daño que recibe cada objeto y asi poder descontar esto de la salud 
+    de ese objeto lo que se ase es rebicir como parametro el ataque que le causo el otro objetos a este 
+    y con esto y la defensa mirar si la defensa es mayor que el ataque que recivio no se le descontara salud
+    pero si el ataque es mayor que la defensa se decontara salud*/
     public void RecibirAtaque(int ataqueRecibido)
     {
         int defensaAtaque = Defensa();
@@ -72,7 +76,7 @@ public class Pokemon:IPokemon{
             int diferencia = Math.Max(0,ataqueRecibido - defensaAtaque);
             int nuevaSalud = saludPokemon - diferencia;
             SetSalud(nuevaSalud);
-            Console.WriteLine($"{nombrePokemon} ha recibido {diferencia} puntos de daño!");
+            Console.WriteLine($"{nombrePokemon} ha recibido {diferencia} puntos de daño y queda con {GetSalud()} puntos de vida");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 
 class Program{
     static void Main(string[] args){
+        //se pide que se ingresen los valores del primer pokemon
         Console.WriteLine("=============Ingrese los datos del primer Pokemon================");
         Console.Write("Ingrese el nombre del pokemon: ");
         String NombrePokemon = Console.ReadLine();
@@ -10,7 +11,7 @@ class Program{
         Console.Write("Ingrese la salud del pokemon: ");
         int SaludPokemon = int.Parse(Console.ReadLine());
         Pokemon pokemon1 = new Pokemon(NombrePokemon,TipoPokemon,SaludPokemon);
-
+        //se pide que se ingresen los valores del segundo pokemon
         Console.WriteLine("=============Ingrese los datos del segundo Pokemon================");
         Console.Write("Ingrese el nombre del pokemon: ");
         String NombrePokemon2 = Console.ReadLine();
@@ -20,7 +21,9 @@ class Program{
         int SaludPokemon2 = int.Parse(Console.ReadLine());
         Pokemon pokemon2 = new Pokemon(NombrePokemon2,TipoPokemon2,SaludPokemon2);
         Console.WriteLine("");
-        
+        /*se simula la batalla en la cual en una variable se guarda el valor que retorna el metodo ataque para
+        posteriormente pasar este valor al metodo recibirataque en los parametros y asi poder realizar los
+        calculos para poder descontar o no salud*/
         for(int i = 0; i < 3; i++){
             Console.WriteLine($"*********Round-{i+1}*********");
             int ataquePokemon1 = pokemon1.Ataque();
@@ -32,7 +35,8 @@ class Program{
             Console.WriteLine($"{pokemon2.GetNombre()} ataca a {pokemon1.GetNombre()}!");
             pokemon1.RecibirAtaque(ataquePokemon2);
         }
-
+        /*Finalmente despues de los 3 rounds se compara la salud para determinar cual de los dos pokemones
+        Es el ganador*/
         if (pokemon1.GetSalud() > pokemon2.GetSalud())
         {
             Console.WriteLine($"Gana {pokemon1.GetNombre()}! con {pokemon1.GetSalud()} puntos de vida");
